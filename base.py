eb8f96546350e86f829392885682e00e1c8b9dad
Francis318 <enriquereyes32@hotmail.com>
2023-07-11 23:14:43 -0600

    Creacion de base y base2

---
+++ base.py
@@ -0,0 +1,21 @@
+def  create_file(file_name,content=None):
+    #"""Crea archivos sin contenido"""
+    mode="w" if content else "x"
+    file= open(file_name,mode)
+    if content:
+        file.write(content)
+    file.close()
+
+create_file("sample1.txt","contenido de archivo")
+create_file("sample2.txt",)
+
+def modify_file(file_name,content,overwrite=False):
+    #"""modifica el archivo file name
+    # si overwrite es true sobreescribira el archivo"""
+    mode="w" if overwrite else "a"
+    file= open(file_name,mode)
+    file.write(content)
+    file.close
+
+modify_file("sample1.txt","Este contenido fue sobreescrito",overwrite=True)
+modify_file("sample2.txt","Este contenido fue adicionado")
